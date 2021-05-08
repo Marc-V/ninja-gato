2f791297b2cf0d3af4aff48e4354e1b0acb23f59
Marc-V <mvsquez9@gmail.com>
2021-05-07 21:00:54 -0400

    Rompecabezas gatuno

---
+++ ninja-gato.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $(".one").click(function(){
+        $(this).attr("src","images/fila-1-col-1 (1).jpg");
+    });
+
+    $(".two").click(function(){
+        $(this).attr("src","images/fila-2-col-1 (1).jpg");    });
+
+    $(".three").click(function(){
+        $(this).attr("src","images/fila-3-col-1 (1).jpg");
+    });
+
+    $(".four").click(function(){
+        $(this).attr("src","images/fila-4-col-1 (1).jpg");    });
+
+    $(".five").click(function(){
+        $(this).attr("src","images/fila-5-col-1 (1).jpg");    });
+    
+    $(".six").click(function(){
+        $(this).attr("src","images/fila-1-col-1.jpg");    });
+    
+    $(".seven").click(function(){
+        $(this).attr("src","images/fila-2-col-1.jpg");    });
+
+    $(".eight").click(function(){
+        $(this).attr("src","images/fila-3-col-1.jpg");    });
+
+    $(".nine").click(function(){
+        $(this).attr("src","images/fila-4-col-1.jpg");    });
+
+    $(".ten").click(function(){
+        $(this).attr("src","images/fila-5-col-1.jpg");    });
+});
